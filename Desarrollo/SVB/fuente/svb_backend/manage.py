62e3686906714c0674a4771f1c9e81f616b42da4
mrtnayo <mrtnayo@gmail.com>
2020-07-14 22:23:13 -0500

    Implementa servicios autenticación por tokens y gestión de productos.

--- Desarrollo/SVB/fuente/svb_backend/manage.py
+++ Desarrollo/SVB/fuente/svb_backend/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svb_backend.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'svb_backend.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- Desarrollo/SVB/fuente/svb_backend/svb_backend/urls.py
+++ Desarrollo/SVB/fuente/svb_backend/svb_backend/urls.py
@@ -1,21 +1,39 @@
-"""svb_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+admin.site.site_header = "SISTEMA DE VENTA PARA BODEGAS"
+admin.site.site_title = "SISTEMA DE VENTA PARA BODEGAS Admin Portal"
+admin.site.index_title = "BIENVENIDOS AL ADMINISTRADOR DE SVB 1.0"
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SVB API Documentation",
+        default_version='v1',
+        description="Documentación para el API de SVB.",
+        terms_of_service="",
+        contact=openapi.Contact(email="mrtnayo@gmail.com"),
+        license=openapi.License(name="Powered by MAC"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^docs/redoc/$',
+            schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
+    re_path(r'admin/', admin.site.urls),
+    path('auth/', include('apps.rest_auth.urls')),
+    path('api/products/', include('apps.products.urls')),
+    #     path('api/orders/', include('apps.orders.urls'))
 ]
