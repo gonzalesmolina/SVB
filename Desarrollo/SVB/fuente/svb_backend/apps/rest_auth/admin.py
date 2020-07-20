57356a2b97c374ce68ebdfecd5ba7a909cf6dabf
Charlie <tarazona.charlie@gmail.com>
2020-07-20 18:13:28 -0500

    agregando los modelos Adress y UserProfile al admin de la app

--- Desarrollo/SVB/fuente/svb_backend/apps/rest_auth/admin.py
+++ Desarrollo/SVB/fuente/svb_backend/apps/rest_auth/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
+from apps.rest_auth.models import Address, UserProfile
 
-# Register your models here.
+admin.site.register(Address)
+admin.site.register(UserProfile)
