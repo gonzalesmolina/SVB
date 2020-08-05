3ab899fd95e5301f1372fd6da622e33983143092
Charlie <tarazona.charlie@gmail.com>
2020-08-05 15:12:30 -0500

    urls del modelo suppliers

---
+++ Desarrollo/SVB/fuente/svb_backend/apps/suppliers/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps.suppliers.views import (
+    SupplierDetailView,
+    SupplierListView
+)
+
+urlpatterns = [
+    path('suppliers', SupplierListView.as_view(), name='suppliers-list'),
+    path('suppliers/<int:pk>', SupplierDetailView.as_view(),
+         name='suppliers-detail')
+]
