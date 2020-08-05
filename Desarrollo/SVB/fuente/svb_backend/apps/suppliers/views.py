c4012edebc93a917fc7667c6157a10da5473c202
Charlie <tarazona.charlie@gmail.com>
2020-08-05 14:56:46 -0500

    vistas del modelo supplier

--- Desarrollo/SVB/fuente/svb_backend/apps/suppliers/views.py
+++ Desarrollo/SVB/fuente/svb_backend/apps/suppliers/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from apps.common.utils import DefaultPagination
+from apps.suppliers.models import Supplier
+from apps.suppliers.serializers import SupplierSerializer
+
+class SupplierListView(ListCreateAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [AllowAny]
+    pagination_class = DefaultPagination
+
+
+class SupplierDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [AllowAny]
