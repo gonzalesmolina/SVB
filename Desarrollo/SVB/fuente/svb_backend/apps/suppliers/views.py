fab86cd7a8858cac51fb8edd9e28eb196186b400
Charlie <tarazona.charlie@gmail.com>
2020-08-31 00:48:07 -0500

    filtros para proveedores

--- Desarrollo/SVB/fuente/svb_backend/apps/suppliers/views.py
+++ Desarrollo/SVB/fuente/svb_backend/apps/suppliers/views.py
@@ -1,3 +1,5 @@
+from django_filters import rest_framework as filters
+from rest_framework import filters as drfilters
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
@@ -6,14 +8,22 @@ from rest_framework.permissions import AllowAny
 
 from apps.common.utils import DefaultPagination
 from apps.suppliers.models import Supplier
-from apps.suppliers.serializers import SupplierSerializer
+from apps.suppliers.serializers import SupplierSerializer, SupplierListSerializer
 
 
 class SupplierListView(ListCreateAPIView):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+    filter_backends = (filters.DjangoFilterBackend, drfilters.SearchFilter)
+    filter_fields = ('is_active',)
+    search_fields = ('name', 'ruc', 'address')
     permission_classes = [AllowAny]
     pagination_class = DefaultPagination
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return self.serializer_class
+        return SupplierListSerializer
 
 
 class SupplierDetailView(RetrieveUpdateDestroyAPIView):
